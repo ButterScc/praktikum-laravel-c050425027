da9c8b6381fdcbef90241dbb0aaa520ab0ecb0cb
Muhammad Kemal Fasha Ramadhan <kemalf888@gmail.com>
2026-09-15 10:19:27 +0800

    Perbarui Coding

--- database/migrations/2026_09_13_045334_add_email_to_mahasiswa_table.php
+++ database/migrations/2026_09_13_045334_add_email_to_mahasiswa_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
 {
-    Schema::table('mahasiswas', function (Blueprint $table) {
+    Schema::table('mahasiswa', function (Blueprint $table) {
         $table->string('email')->nullable()->after('nama');
     });
 }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
 {
-    Schema::table('mahasiswas', function (Blueprint $table) {
+    Schema::table('mahasiswa', function (Blueprint $table) {
         $table->dropColumn('email');
     });
 }
