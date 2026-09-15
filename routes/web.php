2147be32174af82ce9e4b480b2f5090b5459db28
Muhammad Kemal Fasha Ramadhan <kemalf888@gmail.com>
2026-09-15 10:23:14 +0800

    Perubahan Coding

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return 'Halaman Utama';
+});
+
 Route::get('/artikel', function () {
     return 'Halaman Artikel';
 });
-?>
-
-
